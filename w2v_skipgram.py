35d6143c91ab8bb102a56df9f2b658fe5cb8a451
Yrgarcia <rezepkagarcia@cl.uni-heidelberg.de>
2019-03-07 17:07:54 +0100

    epochs ready

--- w2v_skipgram.py
+++ w2v_skipgram.py
@@ -1,24 +1,25 @@
 import math
 import sys
 import numpy as np
+from word_sim import evaluate
 
-
-class Ngram:
-    def __init__(self, tokens):
-        self.tokens = tokens
-        self.count = 0
-        self.score = 0.0
-
-    def set_score(self, score):
-        self.score = score
-
-    def get_string(self):
-        return '_'.join(self.tokens)
+#
+# class Ngram:
+#     def __init__(self, tokens):
+#         self.tokens = tokens
+#         self.count = 0
+#         self.score = 0.0
+#
+#     def set_score(self, score):
+#         self.score = score
+#
+#     def get_string(self):
+#         return '_'.join(self.tokens)
+#
 
 
 class Corpus:
     def __init__(self, filename, word_phrase_passes, word_phrase_delta, word_phrase_threshold, word_phrase_filename):
-        i = 0
         file_pointer = open(filename, 'r')
 
         all_tokens = []
@@ -31,109 +32,89 @@ class Corpus:
                 if len(token) > 1 and token.isalnum():
                     all_tokens.append(token)
 
-                i += 1
-                if i % 10000 == 0:
-                    sys.stdout.flush()
-                    sys.stdout.write("\rReading corpus: %d" % i)
-
-        sys.stdout.flush()
-        print("\rCorpus read: %d" % i)
+        print("\rCorpus read.")
 
         file_pointer.close()
 
         self.tokens = all_tokens
 
-        for x in range(1, word_phrase_passes + 1):
-            self.build_ngrams(x, word_phrase_delta, word_phrase_threshold, word_phrase_filename)
+        # for x in range(1, word_phrase_passes + 1):
+        #    self.build_ngrams(x, word_phrase_delta, word_phrase_threshold, word_phrase_filename)
 
         self.save_to_file(filename)
 
-    def build_ngrams(self, x, word_phrase_delta, word_phrase_threshold, word_phrase_filename):
-
-        ngrams = []
-        ngram_map = {}
-
-        token_count_map = {}
-        for token in self.tokens:
-            if token not in token_count_map:
-                token_count_map[token] = 1
-            else:
-                token_count_map[token] += 1
-
-        i = 0
-        ngram_l = []
-        for token in self.tokens:
-
-            if len(ngram_l) == 2:
-                ngram_l.pop(0)
-
-            ngram_l.append(token)
-            ngram_t = tuple(ngram_l)
-
-            if ngram_t not in ngram_map:
-                ngram_map[ngram_t] = len(ngrams)
-                ngrams.append(Ngram(ngram_t))
-
-            ngrams[ngram_map[ngram_t]].count += 1
-
-            i += 1
-            if i % 10000 == 0:
-                sys.stdout.flush()
-                sys.stdout.write("\rBuilding n-grams (%d pass): %d" % (x, i))
-
-        sys.stdout.flush()
-        print("\rn-grams (%d pass) built: %d" % (x, i))
-
-        filtered_ngrams_map = {}
-        file_pointer = open(word_phrase_filename + ('-%d' % x), 'w')
-
-        # http://papers.nips.cc/paper/5021-distributed-representations-of-words-and-phrases-and-their-compositionality.pdf
-        i = 0
-        for ngram in ngrams:
-            product = 1
-            for word_string in ngram.tokens:
-                product *= token_count_map[word_string]
-            ngram.set_score((float(ngram.count) - word_phrase_delta) / float(product))
-
-            if ngram.score > word_phrase_threshold:
-                filtered_ngrams_map[ngram.get_string()] = ngram
-                file_pointer.write('%s %d\n' % (ngram.get_string(), ngram.count))
-
-            i += 1
-            if i % 10000 == 0:
-                sys.stdout.flush()
-                sys.stdout.write("\rScoring n-grams: %d" % i)
-
-        sys.stdout.flush()
-        print("\rScored n-grams: %d, filtered n-grams: %d" % (i, len(filtered_ngrams_map)))
-        file_pointer.close()
-
-        # Combining the tokens
-        all_tokens = []
-        i = 0
-
-        while i < len(self.tokens):
-
-            if i + 1 < len(self.tokens):
-                ngram_l = []
-                ngram_l.append(self.tokens[i])
-                ngram_l.append(self.tokens[i+1])
-                ngram_string = '_'.join(ngram_l)
-
-                if len(ngram_l) == 2 and (ngram_string in filtered_ngrams_map):
-                    ngram = filtered_ngrams_map[ngram_string]
-                    all_tokens.append(ngram.get_string())
-                    i += 2
-                else:
-                    all_tokens.append(self.tokens[i])
-                    i += 1
-            else:
-                all_tokens.append(self.tokens[i])
-                i += 1
-
-        print("Tokens combined")
-
-        self.tokens = all_tokens
+    # def build_ngrams(self, x, word_phrase_delta, word_phrase_threshold, word_phrase_filename):
+    #
+    #     ngrams = []
+    #     ngram_map = {}
+    #
+    #     token_count_map = {}
+    #     for token in self.tokens:
+    #         if token not in token_count_map:
+    #             token_count_map[token] = 1
+    #         else:
+    #             token_count_map[token] += 1
+    #
+    #     ngram_l = []
+    #     for token in self.tokens:
+    #
+    #         if len(ngram_l) == 2:
+    #             ngram_l.pop(0)
+    #
+    #         ngram_l.append(token)
+    #         ngram_t = tuple(ngram_l)
+    #
+    #         if ngram_t not in ngram_map:
+    #             ngram_map[ngram_t] = len(ngrams)
+    #             ngrams.append(Ngram(ngram_t))
+    #
+    #         ngrams[ngram_map[ngram_t]].count += 1
+    #
+    #     print("\rn-grams built.")
+    #
+    #     filtered_ngrams_map = {}
+    #     file_pointer = open(word_phrase_filename + ('-%d' % x), 'w')
+    #
+    #     # http://papers.nips.cc/paper/5021-distributed-representations-of-words-and-phrases-and-their-compositionality.pdf
+    #     for ngram in ngrams:
+    #         product = 1
+    #         for word_string in ngram.tokens:
+    #             product *= token_count_map[word_string]
+    #         ngram.set_score((float(ngram.count) - word_phrase_delta) / float(product))
+    #
+    #         if ngram.score > word_phrase_threshold:
+    #             filtered_ngrams_map[ngram.get_string()] = ngram
+    #             file_pointer.write('%s %d\n' % (ngram.get_string(), ngram.count))
+    #
+    #     print("\rScored n-grams. Filtered n-grams: %d" % (len(filtered_ngrams_map)))
+    #     file_pointer.close()
+    #
+    #     # Combining the tokens
+    #     all_tokens = []
+    #     i = 0
+    #
+    #     while i < len(self.tokens):
+    #
+    #         if i + 1 < len(self.tokens):
+    #             ngram_l = []
+    #             ngram_l.append(self.tokens[i])
+    #             ngram_l.append(self.tokens[i+1])
+    #             ngram_string = '_'.join(ngram_l)
+    #
+    #             if len(ngram_l) == 2 and (ngram_string in filtered_ngrams_map):
+    #                 ngram = filtered_ngrams_map[ngram_string]
+    #                 all_tokens.append(ngram.get_string())
+    #                 i += 2
+    #             else:
+    #                 all_tokens.append(self.tokens[i])
+    #                 i += 1
+    #         else:
+    #             all_tokens.append(self.tokens[i])
+    #             i += 1
+    #
+    #     print("Tokens combined")
+    #
+    #     self.tokens = all_tokens
 
     def save_to_file(self, filename):
 
@@ -158,7 +139,6 @@ class Corpus:
         print("\rPreprocessed input file written")
 
         filepointer.close()
-
 
     def __getitem__(self, i):
         return self.tokens[i]
@@ -188,19 +168,12 @@ class Vocabulary:
         words = []
         word_map = {}
 
-        i = 0
         for token in corpus:
             if token not in word_map:
                 word_map[token] = len(words)
                 words.append(Word(token))
             words[word_map[token]].count += 1
 
-            i += 1
-            if i % 10000 == 0:
-                sys.stdout.flush()
-                sys.stdout.write("\rBuilding vocabulary: %d" % len(words))
-
-        sys.stdout.flush()
         print("\rVocabulary built: %d" % len(words))
 
         self.words = words
@@ -287,9 +260,15 @@ def save(vocab, nn0, filename):
     file_pointer.close()
 
 
+def log_spearman(spearman_corr, filename):
+    with open(filename, "a") as sl:
+        sl.write(str(spearman_corr)+"\n")
+        sl.close()
+
+
 if __name__ == '__main__':
 
-    for input_filename in ['tokenized_en.txt']:
+    for input_filename in ['TEST_en']:
     #for input_filename in ['news-2012-phrases-10000.txt']:
 
         # Number of negative examples
@@ -327,31 +306,15 @@ if __name__ == '__main__':
                 nn1 = np.zeros(shape=(len(vocab), dim))
 
                 # Initial learning rate
-                initial_alpha = 0.01 # 0.01
+                initial_alpha = 0.02  # 0.01
 
                 # Modified in loop
-                global_word_count = 0
                 alpha = initial_alpha
-                word_count = 0
-                last_word_count = 0
-
                 tokens = vocab.indices(corpus)
 
-                for epoch in 15:
+                for epoch in range(15):
                     print("\rTraining epoch %d..." % epoch)
                     for token_idx, token in enumerate(tokens):
-                        if word_count % 10000 == 0:
-                            global_word_count += (word_count - last_word_count)
-                            last_word_count = word_count
-
-                            # Recalculate alpha
-                            # alpha = initial_alpha * (1 - float(global_word_count) / len(corpus))
-                            # if alpha < initial_alpha * 0.0001:
-                            #     alpha = initial_alpha * 0.0001
-
-                            sys.stdout.flush()
-                            sys.stdout.write("\rTraining: %d of %d" % (global_word_count, len(corpus)))
-
                         # Randomize window size, where win is the max window size
                         current_window = np.random.randint(low=1, high=window+1)
                         context_start = max(token_idx - current_window, 0)
@@ -372,12 +335,10 @@ if __name__ == '__main__':
                             # Update nn0
                             nn0[context_word] += neu1e
 
-                        word_count += 1
-
-                    global_word_count += (word_count - last_word_count)
-                    sys.stdout.flush()
-
-
+                    alpha = 0.8 ** epoch * initial_alpha
+                    save(vocab, nn0, 'BASICoutput-%s-%d-%d-%d' % (input_filename, window, dim, word_phrase_passes))
+                    sp = evaluate('BASICoutput-%s-%d-%d-%d' % (input_filename, window, dim, word_phrase_passes), "globalSim")
+                    log_spearman(sp, "LOG_BASIC")
                 # Save model to file
                 save(vocab, nn0, 'BASICoutput-%s-%d-%d-%d' % (input_filename, window, dim, word_phrase_passes))
 
